f8e28d76e87c66f4b329060302e78efdf8b2d857
15ec014 <gowthamsmart98@gmail.com>
2018-08-13 15:34:08 +0530

    Create prob.c

---
+++ prob.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#included<conio.h>
+void main()
+{
+char a[30];
+char c;
+int i;
+clrscr();
+scanf("%s",a);
+for(i=0;a[i]!=NULL;i++)
+{
+if(a[i]==c)
+{
+printf("the position is:%d",i);
+}
+}
+}
